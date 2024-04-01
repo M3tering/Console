cdfcf9490e78bd53af0827bdcfd6902c2e160421
ichristwin <ichristwin@gmail.com>
2024-04-01 13:17:26 +0100

    changed contract input json names

--- src/logic/warp.ts
+++ src/logic/warp.ts
@@ -7,7 +7,7 @@ const warp = WarpFactory.forMainnet()
   .use(new Ed25519Extension())
   .use(new EthersExtension());
 
-export async function interact(contractId: string, data: Payload) {
+export async function interact(contractId: string, payload: Payload) {
   const contractLabel = process.env.CONTRACT_LABEL || "M3ters";
   const tags = [
     { name: "Contract-Label", value: contractLabel } as Tag,
@@ -19,20 +19,19 @@ export async function interact(contractId: string, data: Payload) {
     .connect(await warp.arweave.wallets.generate());
 
   const result = await contract.dryWrite(
-    { data, function: "meter" },
+    { payload, function: "meter" },
     undefined,
     tags
   );
 
   const state = result.state as State;
 
-  const payload = JSON.parse(data[0]);
-  const deviceNonce: number = payload[0];
+  const deviceNonce: number = JSON.parse(payload[0])[0];
   const nonce: number =
     deviceNonce > state.nonce ? deviceNonce + 1 : state.nonce + 1;
 
   if (result.type === "ok") {
-    await contract.writeInteraction({ data, function: "meter" }, { tags });
+    await contract.writeInteraction({ payload, function: "meter" }, { tags });
     return { is_on: state.is_on } as State;
   } else if (deviceNonce === 0) {
     return { nonce, is_on: true } as State;
