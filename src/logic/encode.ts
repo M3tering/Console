1c9bdf8954aaf48f36550189d1b4508603fdf11a
I. Christwin <iChristwin@gmail.com>
2025-03-19 08:13:00 +0100

    Reverting to old encoding that ignores location

--- src/logic/encode.ts
+++ src/logic/encode.ts
@@ -1,29 +1,18 @@
 import { State } from "../types";
 
-function intToByteArray(num: number) {
-  const byteArray = new Uint8Array(4);
-  byteArray[0] = (num >> 24) & 0xff; // Most significant byte
-  byteArray[1] = (num >> 16) & 0xff;
-  byteArray[2] = (num >> 8) & 0xff;
-  byteArray[3] = num & 0xff; // Least significant byte
-  return Array.from(byteArray);
-}
-
-function floatToByteArray(float: number) {
-  const buffer = new ArrayBuffer(4); // 4 bytes for a float (32-bit)
-  const view = new DataView(buffer);
-  view.setFloat32(0, float, false); // false for Big Endian
-  return Array.from(new Uint8Array(buffer));
-}
-
-export function encode(state: State, latitude: number, longitude: number) {
-  let responseBytes = floatToByteArray(latitude).concat(
-    floatToByteArray(longitude)
-  );
+export function encode(state: State) {
   let nonce = state.nonce;
+  let byteArray: number[] = [];
+
   if (nonce) {
-    responseBytes = intToByteArray(nonce).concat(responseBytes);
+    byteArray = [0, 0, 0, 0];
+    for (let index = 0; index < 4; index++) {
+      let byte = nonce & 0xff;
+      byteArray[3 - index] = byte;
+      nonce = (nonce - byte) / 256;
+    }
   }
-  responseBytes.unshift(state.is_on ? 1 : 0);
-  return responseBytes;
+  const status = state.is_on ? 1 : 0;
+  byteArray.unshift(status);
+  return byteArray;
 }
