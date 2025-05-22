1ab47f8d65411e8f2da7170511e7825a372c19f6
Godwin <godwinaquinas@gmail.com>
2025-05-22 22:28:28 +0100

    fix tag error

--- src/logic/warp.ts
+++ src/logic/warp.ts
@@ -36,7 +36,7 @@ export async function interact(m3terId: string, lastNonce: number, payload: Payl
           { name: "Contract-Label", value: contractLabel },
           { name: "Contract-Use", value: "M3tering Protocol" },
           { name: "Content-Type", value: "application/json" },
-          { name: "M3ter-ID", value: m3terId },
+          // { name: "M3ter-ID", value: m3terId },
         ]
       }
     });
