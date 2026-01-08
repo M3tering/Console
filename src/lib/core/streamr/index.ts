70cdaab621b02e53b2ae2f34362e7be97cd0b6fe
Emmo00 <nwaforemmanuel005@gmail.com>
2026-01-08 19:45:33 +0100

    refactor: modularize Streamr client initialization, destroy client instance after action and enhance transaction publishing with retry logic

--- src/lib/core/streamr/index.ts
+++ src/lib/core/streamr/index.ts
@@ -1,5 +1,5 @@
 import { StreamrClient } from "@streamr/sdk";
-import { buildBatchPayload } from "../../utils";
+import { buildBatchPayload, retry } from "../../utils";
 import type { Hooks, TransactionRecord } from "../../../types";
 
 const { STREAMR_STREAM_ID, ETHEREUM_PRIVATE_KEY } = process.env;
@@ -8,28 +8,14 @@ if (!STREAMR_STREAM_ID || !ETHEREUM_PRIVATE_KEY) {
   throw new Error("Missing STREAMR_STREAM_ID or ETHEREUM_PRIVATE_KEY in environment variables");
 }
 
-export const streamrClient = new StreamrClient({
-  auth: {
-    privateKey: ETHEREUM_PRIVATE_KEY,
-  },
-});
-
-const stream = streamrClient.getStream(STREAMR_STREAM_ID);
-
-stream
-  .then((stream) => {
-    console.log(`[Streamr] Connected to stream: ${stream.id}`);
-  })
-  .catch((error) => {
-    console.error("[Streamr] Error connecting to stream:", error);
-  });
-
 export default class implements Hooks {
   async onTransactionDistribution(_: any, __: any, pendingTransactions: TransactionRecord[]) {
     // send pending transactions to streamr
     try {
       console.info(`Sending pending transactions to streamr`);
-      await publishPendingTransactionsToStreamr(pendingTransactions);
+
+     await retry(() => publishPendingTransactionsToStreamr(pendingTransactions), 3);
+
       console.info(`Successfully sent pending transactions to streamr`);
     } catch (error) {
       console.error(`Error sending pending transactions to streamr: ${error}`);
@@ -37,15 +23,19 @@ export default class implements Hooks {
   }
 }
 
-async function getStream() {
-  return await stream;
-}
-
-async function publishToStream(data: any) {
-  const stream = await getStream();
-  await stream.publish(data);
-}
-
 async function publishPendingTransactionsToStreamr(pendingTransactions: TransactionRecord[]) {
-  await publishToStream(buildBatchPayload(pendingTransactions));
+  const streamrClient = new StreamrClient({
+    auth: {
+      privateKey: ETHEREUM_PRIVATE_KEY!,
+    },
+  });
+
+  const stream = await streamrClient.getStream(STREAMR_STREAM_ID!);
+
+  const batchPayload = buildBatchPayload(pendingTransactions);
+
+  await stream.publish(batchPayload);
+
+  // destroy the client to free resources
+  await streamrClient.destroy();
 }
