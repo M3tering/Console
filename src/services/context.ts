0b4e405f9fe8d2bd158b17bd522fb16197405025
Emmo00 <nwaforemmanuel005@gmail.com>
2026-03-03 19:38:47 +0100

    fix: update contract addresses for m3ter and rollup to use protocol ens

--- src/services/context.ts
+++ src/services/context.ts
@@ -127,13 +127,13 @@ server.listen(port, () => {
 export const provider = new JsonRpcProvider(process.env.MAINNET_RPC);
 
 export const m3ter = new Contract(
-  process.env.M3TER_CONTRACT_ADDRESS || "0x9C547B649475f1bE81323AefdbcF209C17961D5E",
+  process.env.M3TER_CONTRACT_ADDRESS || "heads.m3ter.eth",
   ["function publicKey(uint256) view returns (bytes32)", "function tokenID(bytes32) view returns (uint256)"],
   provider,
 );
 
 export const rollup = new Contract(
-  process.env.ROLLUP_CONTRACT_ADDRESS || "0xf8f2d4315DB5db38f3e5c45D0bCd59959c603d9b",
+  process.env.ROLLUP_CONTRACT_ADDRESS || "rollup.m3ter.eth",
   ["function nonce(uint256) external view returns (bytes6)"],
   provider,
 );
